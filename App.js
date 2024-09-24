5c300bc605b5a6987c3776e14f66e10a43324203
PavitarSharma <pavitarsharma144@gmail.com>
2024-09-24 16:05:47 +0530

    News App

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return <DrawerNavigator />;
+};
+export default App;
